3b5dc1679a0fb881eac479d1bf0528123d3838af
Josh Maccabee <josh@yipit.com>
2019-12-04 10:19:53 -0500

    Day4/part1 first pass.

--- puzzles/day4/part1.py
+++ puzzles/day4/part1.py
@@ -1,1 +1,61 @@
 PUZZLE_INPUT = "372304-847060"
+
+
+def parse_password_int_range():
+    password_int_range = tuple(
+        int(int_string) for int_string in 
+        PUZZLE_INPUT.split()
+    )
+    return range(*password_int_range)
+
+
+def is_valid_password(password):
+    REQUIRED_LENGTH = 6
+    password_int_range = parse_password_int_range()
+
+    correct_length = (len(str(password)) == REQUIRED_LENGTH)
+    within_correct_window = (
+        (password >= password_int_range[0]) & 
+        (password <= password_int_range[-1])
+    )
+    not_decreasing = is_never_decreasing(password)
+    has_repeat_integer = has_repeated_integer(password)
+
+    return (
+        correct_length & 
+        within_correct_window & 
+        increasing & 
+        has_double
+    )
+
+
+def is_never_decreasing(password):
+    password = str(password)
+    is_increasing = True
+    i = 0
+    for i in range(len(password)):
+        first = password[i]
+        second = password[i+1]
+        if second < first:
+            is_increasing = False
+    return is_increasing
+
+
+def has_repeated_integer(password):
+    password = str(password)
+    has_repeat = False
+    for i in range(len(password)):
+        first = password[i]
+        second = password[i+1]
+        if first == second:
+            has_repeat = True
+    return has_repeat    
+
+
+if __name__ == '__main__':
+    password_int_range = parse_password_int_range()
+    valid_passwords = [
+        i for i in password_int_range
+        if is_valid_password(i)
+    ]
+    print('Number of valid passwords:', len(valid_passwords))
