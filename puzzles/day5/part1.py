762b4c51e3bfa9c0e751a0d3074e3717b519dd99
Josh Maccabee <josh@yipit.com>
2019-12-06 11:38:10 -0500

    Day5: wow this got hard.

--- puzzles/day5/part1.py
+++ puzzles/day5/part1.py
@@ -4,34 +4,82 @@ def read_inputs():
         input_string = input_file.read()
 
     return [
-        int(i) for i in input_string.split(',')
+        i for i in input_string.split(',')
     ]
 
 
-class UnhandledOpcode(Exception):
-    """
-    This exception should never be raised, 
-    but we implement it here just in case.
-    """
-    pass
+def run_program(memory):
+    TERMINATE_OPCODE = "99"
+    ADDITIVE_OPCODE = "01"
+    MULTIPLICATIVE_OPCODE = "02"
+    STORE_OPCODE = "03"
+    RETURN_OPCODE = "04"
+
+    pointer = 0
+    terminated = False
+    while not terminated:
+        instruction = memory[pointer]
+        opcode, param2_mode, param1_mode, param0_mode = parse_instruction(
+            instruction,
+        )
+
+        if opcode == TERMINATE_OPCODE:
+            terminated = True
+            break
+        
+        pointer += 1
+        param0 = memory[pointer]
+        param1 = memory[pointer+1]
+        param2 = memory[pointer+2]
+
+        param0_value = parse_value(param0_mode, param0, memory)
+        param1_value = parse_value(param1_mode, param1, memory)
+        param2_value = parse_value(param2_mode, param2, memory)
+
+        if opcode == ADDITIVE_OPCODE:
+            output = param0_value + param1_value
+            memory[param2_value] = output
+            pointer += 3
+
+        elif opcode == MULTIPLICATIVE_OPCODE:
+            output = param0_value + param1_value
+            memory[param2_value] = output            
+            pointer += 3
+
+        elif opcode == STORE_OPCODE:
+            memory[param0_value] = param0_value
+            pointer += 1
+
+        elif opcode == RETURN_OPCODE:
+            return memory[param0_value]
 
 
-def handle_opcode(opcode, pos_one, pos_two=None):
-    # handle the opcode and positional arguments
-    ADDITIVE_OPCODE = 1
-    MULTIPLICATIVE_OPCODE = 2
-    STORE_INPUT_OPCODE = 3
-    OUTPUT_OPCODE = 4
-    
-    if opcode == ADDITIVE_OPCODE:
-        return pos_one + pos_two
-    elif opcode == MULTIPLICATIVE_OPCODE:
-        return pos_one * pos_two
-    elif opcode == STORE_INPUT_OPCODE:
-        pass
-    elif opcode == OUTPUT_OPCODE:
-        pass
-    else:
-        # if we see an opcode we don't expect,
-        # raise an error
-        raise UnhandledOpcode()
+def parse_instruction(instruction):
+    # ensure instruction is 5 characters long;
+    # if shorter than 5 characters, add leading 0s
+    INSTRUCTION_LENGTH = 5
+    leading_zeroes = INSTRUCTION_LENGTH - len(instruction)
+    instruction = (leading_zeroes * "0") + instruction
+
+    opcode = instruction[-2:]
+    param2_mode, param1_mode, param0_mode = [
+        int(i) for i in instruction[:3]
+    ]
+
+    return opcode, param2_mode, param1_mode, param0_mode
+
+
+def parse_value(mode, param, memory):
+    POSITION_MODE = 0
+    IMMEDIATE_MODE = 1
+
+    param = int(param)
+    if mode == POSITION_MODE:
+        return int(memory[param])
+    elif mode == IMMEDIATE_MODE:
+        return param
+
+
+if __name__ == '__main__':
+    TEST_INPUT = "1,3,0,4,0,99".split(',')
+    assert run_program(TEST_INPUT) == 0
